4a1b805aae0be22e240e6e84b1a5125ef3a38622
nmarkov <nikita.markov@ailabs.ru>
2016-06-18 21:09:34 +0300

    wip

--- app.js
+++ app.js
@@ -31,21 +31,6 @@ app.use('/users', users);
 app.use('/api', api);
 app.use('/cars', cars);
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Pass to next layer of middleware
-  next();
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
